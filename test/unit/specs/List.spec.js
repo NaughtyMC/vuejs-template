bc2cf055a1410581c7e16345b379c2110f547e91
Will McNaughton <wsmcnaughton@gmail.com>
2017-09-26 10:33:58 -0500

    Update Unit tests to work with Vuex

--- test/unit/specs/List.spec.js
+++ test/unit/specs/List.spec.js
@@ -1,34 +1,19 @@
 import List from '@/components/List';
-import Vue from 'vue';
+import store from '@/store';
 import { mount } from 'avoriaz';
 
 describe('List.vue', () => {
-	it('displays items from the list', () => {
-		const Constructor = Vue.extend(List);
-		const ListComponent = new Constructor().$mount();
+	let ListComponent;
 
-		expect(ListComponent.$el.textContent).to.contain('sleep');
+	beforeEach(() => {
+		ListComponent = mount(List, { store });
 	});
 
-	it('adds a new item to list on click', () => {
-		const Constructor = Vue.extend(List);
-		const ListComponent = new Constructor().$mount();
-
-		ListComponent.newItem = 'brush my teeth';
-
-		const button = ListComponent.$el.querySelector('button');
-
-		const clickEvent = new window.Event('click');
-		button.dispatchEvent(clickEvent);
-		ListComponent._watcher.run(); // eslint-disable-line no-underscore-dangle
-
-		expect(ListComponent.$el.textContent).to.contain('brush my teeth');
-		expect(ListComponent.listItems).to.contain('brush my teeth');
+	it('displays items from the list', () => {
+		expect(ListComponent.text()).to.contain('sleep');
 	});
 
 	it('adds new item to list on click with avoriaz', () => {
-		const ListComponent = mount(List);
-
 		ListComponent.setData({
 			newItem: 'brush my teeth',
 		});
@@ -37,6 +22,6 @@ describe('List.vue', () => {
 		button.trigger('click');
 
 		expect(ListComponent.text()).to.contain('brush my teeth');
-		expect(ListComponent.data().listItems).to.contain('brush my teeth');
+		expect(ListComponent.data().tasks).to.contain('brush my teeth');
 	});
 });
